5c533682218a4675e971f5fbc3606437246bc7fb
Dave Dopson (epic) <ddopson@epicapplications.com>
2011-12-17 20:13:42 -0800

    adding targets

---
+++ targets/Android/src/com/epic/framework/cfg/EpicProjectConfig.java
@@ -0,0 +1,28 @@
+package com.epic.framework.cfg;
+
+import com.epic.framework.common.Ui.EpicScreen;
+import com.epic.framework.common.Ui.EpicSound;
+import com.epic.framework.common.types.Dimension;
+
+	public class EpicProjectConfig {
+	public static void onApplicationStart() { }
+
+	public static EpicScreen getInitialScreenObject(String screen, String extra) {
+		return null;
+	}
+
+	public static EpicSound getBackgroundMusic() {
+		return null;
+	}
+	
+	public static boolean getIsTitlebarDisabled() {
+		return true;
+	}
+	
+	public static boolean isReleaseMode = true;
+
+	public static final Dimension designDimensions = new Dimension(800, 480);
+	public static Dimension getDesignDimensions() {
+		return designDimensions;
+	}
+}
