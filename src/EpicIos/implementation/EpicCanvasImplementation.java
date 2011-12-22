5fb2f2ff14824473b861f1553986da30ec44c8d8
Dave Dopson (epic) <ddopson@epicapplications.com>
2011-12-22 12:39:15 -0800

    remove excess logging

--- src/EpicIos/implementation/EpicCanvasImplementation.java
+++ src/EpicIos/implementation/EpicCanvasImplementation.java
@@ -50,7 +50,6 @@ public class EpicCanvasImplementation {
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public static void drawBitmapImpl(Object graphicsObject, Object bitmapObject, int x, int y, int alpha, int sx, int sy, int sw, int sh) {
-//		EpicLog.v("EpicCanvasImplementation.drawBitmap");
 		CGContext c = (CGContext) graphicsObject;
 		UIImage uiimg = (UIImage) bitmapObject;
 		if(alpha == EpicCanvas.NO_ALPHA) {
@@ -60,15 +59,12 @@ public class EpicCanvasImplementation {
 		}
 		//		c.drawImage(getDstRect(x, y, sw, sh), uiimg.getCGImage());
 		uiimg.drawInRect(getDstRect(x, y, sw, sh));
-//		EpicLog.v("EpicCanvasImplementation.drawBitmap - done");
 	}	
 
 	public static void drawCircle(Object graphicsObject, int color, int alpha, int x_center, int y_center, int radius) {
-		EpicLog.v("EpicCanvasImplementation.drawCircle");
 		CGContext c = (CGContext) graphicsObject;
 		c.setFillColor(getColorFloatsFromInt(color));
 		c.fillEllipseInRect(new CGRect(x_center - radius, y_center - radius, radius * 2, radius * 2));
-		EpicLog.v("EpicCanvasImplementation.drawCircle - done");
 	}
 
 	public static void drawBorder(Object graphicsObject, int left, int top, int width, int height, int color, int size) {
@@ -78,12 +74,10 @@ public class EpicCanvasImplementation {
 	}
 
 	public static void applyFill(Object graphicsObject, int left, int top, int right, int bottom, int color) {
-		EpicLog.v("EpicCanvasImplementation.applyFill");
 		CGContext c = (CGContext) graphicsObject;
 		c.setFillColor(getColorFloatsFromInt(color));
 		c.setAlpha(1.0f);
 		c.fillRect(new CGRect(left, top, right - left, bottom - top));
-		EpicLog.v("EpicCanvasImplementation.applyFill - done");
 	}
 
 	public static void drawLine(Object graphicsObject, int x, int y, int x2, int y2, int strokeWidth, int color) {
@@ -95,15 +89,12 @@ public class EpicCanvasImplementation {
 	public static void drawText(Object graphicsObject, String text, int left, int top, EpicFont font, int color, int rotateBy) {
 		CGContext c = (CGContext) graphicsObject;
 		EpicFontImplementation fi = (EpicFontImplementation)font.fontObject;
-		EpicLog.v("EpicCanvasImplementation.drawText(" + StringHelper.namedArgList("text", text, "left", left, "top", top, "font", fi.name, "size", fi.size, "color", color) + ")");
+//		EpicLog.v("EpicCanvasImplementation.drawText(" + StringHelper.namedArgList("text", text, "left", left, "top", top, "font", fi.name, "size", fi.size, "color", color) + ")");
 		c.setFillColor(getColorFloatsFromInt(color));
 		c.setStrokeColor(getColorFloatsFromInt(color));
 		c.setTextDrawingMode(0);
 		c.selectFont(fi.name, (float)fi.size);
 		c.showTextAtPoint((float)left, (float)top + fi.size, text);
-		CGPoint p = c.getTextPosition();
-
-		EpicLog.v("EpicCanvasImplementation.drawText - done.  p=" + p.x + ", " + p.y);
 	}
 
 	public static void drawText(Object graphicsObject, char[] buffer, int lineStart, int i, int left, int top, EpicFont font, int color, int rotateBy) {
