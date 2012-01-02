47298725717e25313c984063dc0a9cbb3d9ebb54
Dave Dopson (epic) <ddopson@epicapplications.com>
2012-01-02 01:12:13 -0800

    fix bitmap scaling - HUGE PERF BUMP

--- src/EpicIos/implementation/EpicBitmapImplementationNative.java
+++ src/EpicIos/implementation/EpicBitmapImplementationNative.java
@@ -1,5 +1,6 @@
 package com.epic.framework.implementation;
 
+import org.xmlvm.iphone.CGPoint;
 import org.xmlvm.iphone.UIImage;
 
 import com.epic.framework.common.util.exceptions.EpicNativeMethodMissingImplementation;
@@ -7,5 +8,9 @@ import com.epic.framework.common.util.exceptions.EpicNativeMethodMissingImplemen
 public class EpicBitmapImplementationNative {
 	public static UIImage resizeImage(UIImage src, int width, int height) {
 		throw new EpicNativeMethodMissingImplementation("EpicBitmapImplementationNative");
+	}
+	
+	public static void drawAtPoint(UIImage image, CGPoint point) {
+		throw new EpicNativeMethodMissingImplementation("EpicBitmapImplementationNative");		
 	}
 }
