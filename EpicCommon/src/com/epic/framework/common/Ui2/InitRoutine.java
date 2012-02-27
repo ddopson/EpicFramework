3f4ef89ce469937ca7f67f5113347bc6af322746
Dave Dopson (epic) <ddopson@epicapplications.com>
2012-02-26 22:28:03 -0800

    adding back the hacky InitRoutine for now

--- EpicCommon/src/com/epic/framework/common/Ui2/InitRoutine.java
+++ EpicCommon/src/com/epic/framework/common/Ui2/InitRoutine.java
@@ -1,30 +1,23 @@
-//package com.epic.framework.common.Ui2;
-//
-//import java.io.InputStream;
-//
-//import com.epic.framework.common.Ui.EpicBitmap;
-//import com.epic.framework.common.Ui2.JSON.JSONException;
-//import com.epic.framework.common.Ui2.JSON.JSONTokener;
-//import com.epic.resources.EpicImages;
-//
-//public class InitRoutine {
-//	public static void init() throws JSONException {
-//		ClassEpicButtonWidget.register();
-//		ClassEpicBitmap.register();
-//		ClassEpicAction.register();
-//		ClassEpicClass.register();
+package com.epic.framework.common.Ui2;
+
+import java.io.InputStream;
+
+import com.epic.framework.common.Ui.EpicBitmap;
+import com.epic.framework.common.Ui2.JSON.JSONException;
+import com.epic.framework.common.Ui2.JSON.JSONTokener;
+
+public class InitRoutine {
+	public static void init() throws JSONException {
+		ClassEpicButtonWidget.register();
+		ClassEpicBitmap.register();
+		ClassEpicAction.register();
+		ClassEpicClass.register();
 //		ClassEnsureLoginAction.register();
 //		ClassStartGameAction.register();
-//		ClassEpicScreenObject.register();
-//		ClassEpicWidget.register();
-//		ClassEpicBackgroundWidget.register();
-//		
+		ClassEpicScreenObject.register();
+		ClassEpicWidget.register();
+		ClassEpicBackgroundWidget.register();
+		
 //		EpicImages.init();
-//		
-//		InputStream in = FakeMainMenu.class.getResourceAsStream("testdata.json");
-//		JSONTokener tokenizer = new JSONTokener(in);
-//		Object o = tokenizer.nextValue();
-//		Registry.processJSON(o);
-//
-//	}
-//}
+	}
+}
--- EpicDesktop/src/com/epic/framework/implementation/DesktopMainUiV2.java
+++ EpicDesktop/src/com/epic/framework/implementation/DesktopMainUiV2.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import com.epic.framework.common.Ui.EpicPlatform;
 import com.epic.framework.common.Ui.EpicScreen;
 //import com.epic.framework.common.Ui2.InitRoutine;
+import com.epic.framework.common.Ui2.InitRoutine;
 import com.epic.framework.common.Ui2.Registry;
 import com.epic.framework.common.Ui2.JSON.JSONException;
 
@@ -15,6 +16,7 @@ public class DesktopMainUiV2 {
 		EpicPlatformConfig.platform = EpicPlatform.PLATFORM_IOS;
 		EpicBitmapImplementation.magicBaseDirectory = "./resources";
 		EpicSimulator.currentScreenSize = new Dimension(1024, 768);
+		InitRoutine.init();
 		DesktopMain.MainMethod(args);
 //		EpicPlatform.changeScreen((EpicScreen)Registry.get("screens/MainMenu"));
 	}
