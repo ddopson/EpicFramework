bea61af0cc728e5c6fd6fa3afedd51d90978351d
Dave Dopson (epic) <ddopson@epicapplications.com>
2012-04-12 21:32:21 -0700

    missed file for inheritance support

--- EpicJson/src/com/epic/framework/common/EpicInflatableClass.java
+++ EpicJson/src/com/epic/framework/common/EpicInflatableClass.java
@@ -1,5 +1,11 @@
 package com.epic.framework.common;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
 public @interface EpicInflatableClass {
 	boolean inflatable() default true;
+	boolean ignoreSuperclass() default false;
+	Class<?> arguments() default EpicInflatableClass.class;
 }
