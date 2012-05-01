993f317d2d59cc2ab04b07f49b777a9492200463
Dave Dopson (epic) <ddopson@epicapplications.com>
2012-04-30 22:29:30 -0700

    add NullPlat compat

--- EpicNullPlat/src/com/epic/framework/implementation/EpicBitmapImplementation.java
+++ EpicNullPlat/src/com/epic/framework/implementation/EpicBitmapImplementation.java
@@ -9,5 +9,6 @@ public class EpicBitmapImplementation {
 	public static void recycle(Object platformObject) {	}
 	public static EpicImageInstance loadImageFromUrl(EpicImageFromUrl image) { return null; }
 	public static Object loadBitmap(EpicImageFromResource image, EpicImageInstance instance) { return null; }
+	public static Object scaleImage(EpicImageInstance defaultInstance, EpicImageInstance instance) { return null; }
 
 }
